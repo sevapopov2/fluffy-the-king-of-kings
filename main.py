df21a13e09350cb03858209708f57e67da4fdb63
Vsevolod Popov <sevapopov13@gmail.com>
2023-08-23 16:47:47 +0300

    Window. Add an empty game window with the game title.

---
+++ main.py
@@ -0,0 +1,19 @@
+'''Main file to launch the game.'''
+
+import pygame
+
+from src.consts import GAME_TITLE
+
+
+# Initialize pygame
+pygame.init()
+# Set game title and window size
+pygame.display.set_caption(GAME_TITLE)
+pygame.display.set_mode((400, 500))
+# Check if game is running
+running = True
+# Keep the game running while exit button is not pressed
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
---
+++ src/consts.py
@@ -0,0 +1,3 @@
+'''Constants used in the game project.'''
+
+GAME_TITLE = 'Fluffy, the king of kings!'
